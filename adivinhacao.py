0bbf97ffee0a74c932b9e93ce697abf1399eb38a
Carriel-JT <78246105+Carriel-JT@users.noreply.github.com>
2021-02-09 08:58:15 -0300

    break/continue
    e limitação do intervalo para o usuário

--- adivinhacao.py
+++ adivinhacao.py
@@ -4,13 +4,16 @@ print("*********************************")
 
 numero_secreto = int(43)
 total_de_tentativas = 3
-rodada = 1
 
-while(rodada <= total_de_tentativas):
+for rodada in range(1,total_de_tentativas +1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
-    chute_str = input("digite o seu número: ")
+    chute_str = input("digite um número entre 1 e 100: ")
     print("você digitou ", chute_str)
     chute = int(chute_str)
+
+    if(chute <= 1 or chute >= 100):
+        print("você deve digitar um número entre 1 e 100")
+        continue
 
     acertou = chute == numero_secreto
     maior   = chute > numero_secreto
@@ -18,6 +21,7 @@ while(rodada <= total_de_tentativas):
 
     if(acertou):
         print("você acertou")
+        break
     else:
         if(maior):
             print("você errou! o seu chute foi maior do que o número secreto")
